0d3f85316b7dfa48c06dd8176cd4f995808a111f
Umang <ukhodakiya@gmail.com>
2019-09-25 14:57:49 -0400

    update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAhKhJ7BGulcJ1-sWKwl_Kk_dluz8zRhWU",
+    authDomain: "clientpanel-c80a4.firebaseapp.com",
+    databaseURL: "https://clientpanel-c80a4.firebaseio.com",
+    projectId: "clientpanel-c80a4",
+    storageBucket: "clientpanel-c80a4.appspot.com",
+    messagingSenderId: "583655872429",
+    appId: "1:583655872429:web:8bbe9da61ca996703af324"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAhKhJ7BGulcJ1-sWKwl_Kk_dluz8zRhWU",
+    authDomain: "clientpanel-c80a4.firebaseapp.com",
+    databaseURL: "https://clientpanel-c80a4.firebaseio.com",
+    projectId: "clientpanel-c80a4",
+    storageBucket: "clientpanel-c80a4.appspot.com",
+    messagingSenderId: "583655872429",
+    appId: "1:583655872429:web:8bbe9da61ca996703af324"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
